9b9c720bef48287252a9f9aab5cec2a25c940a9b
andreandrade141 <andrelsandrade43@gmail.com>
2022-10-15 17:53:18 -0300

    Data Cleanup DAG logic.

--- dags/data_cleaning_dag/data_cleaning.py
+++ dags/data_cleaning_dag/data_cleaning.py
@@ -13,3 +13,48 @@ dag_path = os.getcwd()
 def data_cleaning() -> None:
     hotel_data = pd.read_csv()
     hotel_data.head()
+    hotel_data.info()
+    hotel_data.describe()
+
+    hotel_data.isnull().sum()
+
+    nan_replacements = {"children": 0,
+                        "country": "Unknown", "agent": "Organic Booking", "company": "Personal Booking"}
+
+    cleaned_data = hotel_data.fillna(nan_replacements)
+
+    cleaned_data.info()
+
+    cleaned_data.to_csv("", index=False)
+
+
+def cleaned_data_message() -> None:
+    print("Data succesfully cleaned")
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": airflow.utils.dates.days_ago(7)
+}
+
+data_cleaning_dag = DAG(
+    'data_cleaning',
+    default_args=default_args,
+    schedule_interval=timedelta(days=30),
+    catchup=False
+)
+
+clean_data = PythonOperator(
+    task_id="data_cleaning",
+    python_callable=cleaned_data_message,
+    dag=data_cleaning_dag,
+)
+
+message = PythonOperator(
+    task_id="cleaned_data_message",
+    python_callable=cleaned_data_message,
+    dag=data_cleaning_dag
+)
+
+clean_data >> message
